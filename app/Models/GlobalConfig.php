e293dc9f7d7eb5d04b2cd8c319b8a6e2732e7a81
PonrajGithub <apraj.1298@gmail.com>
2026-05-12 11:39:28 +0530

    globalConfig table changes

--- app/Models/GlobalConfig.php
+++ app/Models/GlobalConfig.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class GlobalConfig extends Model
 {
-    protected $fillable = ['year', 'issue', 'reader', 'short_about_us', 'long_about_us'];
+    protected $fillable = ['year', 'issue', 'reader', 'short_about_us', 'long_about_us', 'login_url'];
 }
